7fe87ad2db116125f9b79a58192ed44262033410
Yukta <yuktatn@gmail.com>
2023-06-19 13:07:22 +0530

    add forms and validation

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,19 @@
+// tag::all[]
+// tag::allButValidation[]
+package tacos;
+import java.util.List;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Taco {
+
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
+    private String name;
+
+    @Size(min=1, message="You must choose at least 1 ingredient")
+    private List<String> ingredients;
+
+}
